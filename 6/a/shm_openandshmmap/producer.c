d596eda0d3d40edc55e0c3c5e48eba31bfd08c34
ABHISHEK M B <117739148+MBABHISHEK@users.noreply.github.com>
2024-04-17 11:56:41 +0530

    Update producer.c

--- 6/a/shm_openandshmmap/producer.c
+++ 6/a/shm_openandshmmap/producer.c
@@ -1,23 +1,29 @@
+
+
+//Header files to be linked
 #include<stdio.h>
 #include<stdlib.h>
+#include<unistd.h> //POSIX Library is linked
 #include<string.h>
 #include<fcntl.h>
-#include<unistd.h>
 #include<sys/types.h>
 #include<sys/stat.h>
-#include<sys/mman.h>
 #include<sys/shm.h>
+#include<sys/mman.h>
 #include<sys/ipc.h>
 
 int main() {
     void *shmptr;
-    int shmid = shm_open("OS", O_RDONLY, 0666);
+    char buffer[25];
+    int shmid;
+    shmid = shm_open("OS", O_CREAT|O_RDWR, 0666);
     ftruncate(shmid, 2048);
-    shmptr = mmap(0, 2048, PROT_READ, MAP_SHARED, shmid, 0);
-    printf("\nShared memory is attached at %p\n", shmptr);
-    printf("\nThe string entered to the shared memory is:\n");
-    printf("%s\n", (char *)shmptr);
-    shm_unlink("OS");
+    shmptr = mmap(0, 2048, PROT_WRITE, MAP_SHARED, shmid, 0);
+    printf("\nKey/Id/Fd of the shared memory is %d\n", shmid);
+    printf("Shared memory is attached at %p\n", shmptr);
+    printf("\nEnter the string to be written into the shared memory:\n");
+    read(0, buffer, 25);
+    sprintf(shmptr, "%s", buffer);
 
     return 0;
 }
