8c25bb2ae3d3ae40a476238d8b866006d07bed92
ABHISHEK M B <117739148+MBABHISHEK@users.noreply.github.com>
2024-04-17 12:05:15 +0530

    Update parent.c

--- 7/a/shm_open and mmap/parent.c
+++ 7/a/shm_open and mmap/parent.c
@@ -1,6 +1,5 @@
 
-
-/*program to implement using shmopen and mmap this is parent creating child and distributing wrok to child*/
+/*program to implement using shmopen and mmap*/
 #include<stdio.h>
 #include<stdlib.h>
 #include<unistd.h>
@@ -13,35 +12,23 @@
 #include<sys/mman.h>
 
 int main(int argc, char *argv[]) {
-    int i, k, n1, n2, n3;
-    const int SIZE = 4096;
-    pid_t pid;
-    int shm_fd;
+    int k=2, n1, n2, n3;
     void *shmptr;
+    int shm_fd = shm_open("OS", O_CREAT|O_RDWR, 0666);
+    ftruncate(shm_fd, 4096);
+    shmptr = mmap(0, 4096, PROT_WRITE, MAP_SHARED, shm_fd, 0);
 
-    if (argc > 1) {
-        sscanf(argv[1], "%d", &i);
-        if(i<1) {
-            printf("\nWrong input given!!\n");
-            return 0;
-        }
-    } else {
-        printf("\nN is not passed in the command line!!\n");
-        exit(0);
-    }
-
-    pid = fork();
-    if(pid == 0) {
-        //This is the child part
-        execlp("./fib", "fib", argv[1], NULL);
-    } else if (pid > 0) {
-        wait(NULL);
-        printf("\n[PARENT] Child process completed\n");
-        shm_fd = shm_open("OS", O_RDONLY, 0666);
-        shmptr = mmap(0, SIZE, PROT_READ, MAP_SHARED, shm_fd, 0);
-        printf("\nParent printing:\n");
-        printf("%s\n", (char *)shmptr);
-        shm_unlink("OS");
+    printf("\nChild Printing:\n");
+    int i = atoi(argv[1]);
+    n3 = n1 = 0;
+    n2 = 1;
+    for(k=1; k<=i; k++) {
+        sprintf(shmptr, "%d ", n3);
+        printf("%d ", n3);
+        shmptr += strlen(shmptr);
+        n1 = n2;
+        n2 = n3;
+        n3 = n1+n2;
     }
 
     return 0;
